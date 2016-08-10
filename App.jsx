9c993aa5a52137e6fcfcef81dd8c79f52a7fc1ae
pdebenedictus13 <peter.deben@hotmail.com>
2016-08-10 15:34:30 -0400

    first commit
    creating of first page with title and a button

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class App extends React.Component {
+   render() {
+      return (
+         <div>
+         	<h1>Auto Jeeves</h1>
+         	<button>Continue</button>
+
+         </div>
+      );
+   }
+}
+
+export default App;
